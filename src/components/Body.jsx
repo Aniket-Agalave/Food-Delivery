a2951117e7c0db71f20c02ac351be21b77792874
Aniket-Agalave <aniketsa4510@gmail.com>
2024-07-26 12:43:11 +0530

    Added initail files

---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+const Body = () => {
+  return (
+    <div className="body">
+      <Header />
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="logo">
+        <img src="" alt="" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>about</li>
+          <li>contact us</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
